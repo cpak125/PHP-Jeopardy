cb37717b8c0908efdd985c69de11c16c04fa93c3
Chris Pak <chrispak@Chriss-iMac.attlocal.net>
2022-03-15 14:00:43 -0400

    login function works

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+session_start();
+
+if (isset($_POST['Submit'])) {
+    $accounts = array('admin' => 'admin');
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
+
+    $file = file("accounts.txt");
+    for ($i = 0; $i < count($file); $i++) {
+        $currUser = explode(",", $file[$i]);
+        $currUsername = $currUser[0];
+        $currPassword = $currUser[1];
+        $formattedPassword = rtrim($currPassword);
+        $accounts[$currUsername] = $formattedPassword;
+    }
+    if (isset($accounts[$username]) && $accounts[$username] == $password) {
+        $_SESSION['UserData']['username'] = $accounts[$username]; // password
+        $_SESSION['username'] = $username; // username
+        header("location:board.php");
+        exit;
+    } else {
+        $msg = "<span style='color:#ff0000'>Invalid Login Details</span>";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,10 +44,11 @@
 
     <div id="form">
         <h2>Log in to play</h2>
+        <?php if(isset($msg)) echo $msg; ?> <br>
         <form action="" method="post">
-            <input class="form-input" type="text" name="username" placeholder="Username" value="<?php echo @$_COOKIE['username']; ?>">
-            <input class="form-input" type="password" name="password" placeholder="Password" value="<?php echo @$_COOKIE['Password']; ?>">
-            <input class="form-input" type="submit" name="submit" value="Log in">
+            <input class="form-input" type="text" name="username" placeholder="Username">
+            <input class="form-input" type="password" name="password" placeholder="Password">
+            <input class="form-input" type="submit" name="Submit" value="Log in">
             <p>Not registered? <a href="register.php">Create an account</a></p>
         </form>
     </div>
