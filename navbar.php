52ef0b6432d4e879c63f30f585309a96a3c420e9
Chris Pak <chrispak@Chriss-iMac.local>
2022-03-21 20:43:06 -0400

    added linked project summary page

--- navbar.php
+++ navbar.php
@@ -2,6 +2,7 @@
     <div>Welcome, <?= $_SESSION['username'] ?></div>
     <div>Score: <?= $_SESSION["score"] ?></div>
     <div><a href="leaderboard.php">Leaderboard</a></div>
+    <div><a href="summary.html">Project Summary</a></div>
     <div>
         <a href="logout.php">Log Out</a>
     </div>
